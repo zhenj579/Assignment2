3cfa86b3c4c497f782a1aec972fb6b7072ba1e65
zhenj579 <50351080+zhenj579@users.noreply.github.com>
2022-03-12 21:37:22 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let colorSelected; 
+
+//Adds a row
+function addR() {
+    //alert("Clicked Add Row")
+    let grid = document.getElementById("grid");
+    let rows = document.getElementsByTagName("tr");
+    console.log(rows.length);
+    
+    if (rows.length === 0) {
+        let row = document.createElement("tr");
+        let col = document.createElement("td");
+        col.onclick = function (){
+            this.style.backgroundColor = colorSelected;
+        };
+        row.appendChild(col);
+        grid.appendChild(row);
+
+    }
+}
+//Adds a column
+function addC() {
+    //alert("Clicked Add Col")
+    let cols = document.getElementsByTagName("td");
+    console.log(cols.length); 
+}
+
+//Removes a row
+function removeR() {
+    alert("Clicked Remove Row")
+}
+//Remove a column
+function removeC() {
+    alert("Clicked Remove Col")
+}
+//sets global var for selected color
+function selected(){
+    colorSelected = document.getElementById("selectedID").value;
+    console.log(colorSelected);
+}
+
+function fill(){
+    alert("Clicked Fill All")
+}
+
+function clearAll(){
+    alert("Clicked Clear All")
+}
+
+function fillU(){
+    alert("Clicked Fill All Uncolored")
+}
